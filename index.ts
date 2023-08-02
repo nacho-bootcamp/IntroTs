a306d02450b2f5f752af508c76b436fb84d0cde4
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-08-01 23:41:55 -0300

    Repaso

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
-import { pelicula } from "./src/class/pelicula.class";
+// import { pelicula } from "./src/class/pelicula.class";
 
-const miPelicula: pelicula = new pelicula(1, "robocop", "accion", 56, false)
+// const miPelicula: pelicula = new pelicula(1, "robocop", "accion", 56, false)
+
+
+import { VideoClub } from "./src/class/video.class";
+
+const nachoPelis: VideoClub = new VideoClub("nachoPelis")
+
+nachoPelis.agregar_pelis({ id: 2, titulo: "robocop", duracion: 234, alquilada: false, genero: "guerra" })
--- src/class/video.class.ts
+++ src/class/video.class.ts
@@ -1,6 +1,8 @@
+import { Video } from "../Interface/video.interface"
 import { Catalogo } from "../types/catalogo.type"
+import { pelicula } from "./pelicula.class"
 
-class VideoClub {
+export class VideoClub {
   public name: string
   public catalogo: Catalogo
 
@@ -8,5 +10,9 @@ class VideoClub {
   constructor(name: string) {
     this.name = name
     this.catalogo = []
+  }
+
+  agregar_pelis(datos: Video) {
+    const nuevaPelicula = new pelicula(datos.id, datos.titulo, datos.genero, datos.duracion, datos.alquilada)
   }
 }
