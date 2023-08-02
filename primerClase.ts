5680306c39de372dc73ad05025b05f1965e56769
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-08-01 21:56:45 -0300

    introduccion

---
+++ primerClase.ts
@@ -0,0 +1,25 @@
+const numero: number = 2;
+
+const saludo: string = "hola";
+
+const verdad: boolean = true;
+
+const mi_Objeto: object = {};
+
+const valor_null: null = null;
+
+const valor_undefined: undefined = undefined;
+
+const arrayN: number[] = [1, 23, 423, 543];
+
+const arrayS: string[] = ["chau", "hola"]
+
+// es una funcion en donde definimo que tipo de valor debe ser su parametro
+// y tambien definimos que va a retornar la funcion
+function filtrar(numero: number[]): number[] {
+  return numero.filter(e => e % 2 === 0)
+}
+
+//Con el comando tsc "nombre del archivo.ts"
+//te crea un archivo .js y ese archivo podemos utilizar para
+//ejecutar
